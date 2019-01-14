9b7a77cd979eaecf9913179fdd690b100d7ad1b7
Dingqing Yang <williamyang1203@gmail.com>
2019-01-14 14:08:15 -0800

    add exhaustive grid search for hyperparameter tuning

--- code/example_nonLinear.jl
+++ code/example_nonLinear.jl
@@ -1,37 +1,53 @@
 # Load X and y variable
-using JLD
+using JLD, Random
 data = load("../data/nonLinear.jld")
 (X,y,Xtest,ytest) = (data["X"],data["y"],data["Xtest"],data["ytest"])
 
 # Compute number of training examples and number of features
 (n,d) = size(X)
 
-# shuffle training set using random permutation
-using Random
-shuffled = randperm(n)
-boundary = div(n, 2) # 100 in this case
-train = shuffled[1:boundary]
-val = shuffled[boundary+1:end]
-
-Xtrain = reshape(X[train], boundary, 1)
-ytrain = y[train]
-Xval = reshape(X[val], boundary, 1)
-yval = y[val]
-
 # Fit least squares model
 include("leastSquares.jl")
-lambda = 1
-sigma = 1
-println("Xtrain size", size(Xtrain))
-model = leastSquaresRBFL2(Xtrain,ytrain,lambda,sigma)
+global best_val = Inf
+#println("Xtrain size", size(Xtrain))
+for i = 0.00 : 0.001: 0.02
+	for j = 0.85 : 0.01 : 1 
+		lambda = i
+		sigma = j
+		valErrorSum = 0
+		for k = 1:3
+			# shuffle training set using random permutation
+			shuffled = randperm(n)
+			boundary = div(n, 2) # 100 in this case
+			train = shuffled[1:boundary]
+			val = shuffled[boundary+1:end]
+
+			Xtrain = reshape(X[train], boundary, 1)
+			ytrain = y[train]
+			Xval = reshape(X[val], boundary, 1)
+			yval = y[val]
+
+			model = leastSquaresRBFL2(Xtrain,ytrain,lambda,sigma)
+			yhat = model.predict(Xval)
+			valErrorSum += sum((yhat - yval).^2)/size(Xval,1)
+		end
+		valError = valErrorSum/3
+		if valError < best_val 
+			global best_lambda = lambda
+			global best_sigma = sigma
+			global best_val = valError
+		end
+	end
+end
 
 
 # Report the error on the validation set
 using Printf
-t = size(Xval,1)
-yhat = model.predict(Xval)
-valError = sum((yhat - yval).^2)/t
-@printf("Validation Error = %.2f\n",valError)
+@printf("Validation Error = %.2f\n",best_val)
+@printf("Best lambda = %.3f\n",best_lambda)
+@printf("Best sigma = %.3f\n",best_sigma)
+
+model = leastSquaresRBFL2(Xtrain,ytrain,best_lambda,best_sigma)
 
 # Plot model
 using PyPlot
